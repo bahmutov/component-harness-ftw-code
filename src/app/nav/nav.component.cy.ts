b26ddc4bad46be06ca6b75399b9d597bc0150e78
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-13 11:16:28 -0400

    add cypress component test

---
+++ src/app/nav/nav.component.cy.ts
@@ -0,0 +1,24 @@
+import { NavComponent } from './nav.component'
+import { SigninService } from '../signin.service'
+
+describe('NavComponent', () => {
+  it('should create and show the links', () => {
+    const signinService = new SigninService()
+    cy.spy(signinService, 'login').as('login')
+    cy.spy(signinService, 'logout').as('logout')
+    cy.mount(NavComponent, {
+      componentProperties: {
+        signinService,
+      },
+    })
+    cy.contains('a', 'HOME')
+    cy.contains('button', 'LOG IN').should('be.visible').wait(1000).click()
+    cy.get('@login').should('have.been.called')
+    cy.contains('a', 'HOME')
+    cy.contains('a', 'PROFILE').should('be.visible')
+    cy.contains('button', 'LOG OUT').should('be.visible').wait(1000).click()
+    cy.get('@logout').should('have.been.called')
+    cy.contains('a', 'HOME')
+    cy.contains('button', 'LOG IN').should('be.visible')
+  })
+})
