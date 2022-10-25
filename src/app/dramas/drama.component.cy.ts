7113a568fb519db2543ed79da88bee9c65fc039b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-10-25 10:06:59 -0400

    add list of dramas component test

---
+++ src/app/dramas/drama.component.cy.ts
@@ -0,0 +1,17 @@
+import { DramaService } from './../drama.service'
+import { DramaComponent } from './drama.component'
+import { Drama } from '../models/dramas'
+
+const dramaService = new DramaService()
+const dramas: Drama[] = dramaService.getDramas()
+
+it('shows list of dramas', { viewportHeight: 1000, viewportWidth: 600 }, () => {
+  const template =
+    '<app-drama *ngFor="let d of dramas" [drama]="d"></app-drama>'
+  cy.mount(template, {
+    declarations: [DramaComponent],
+    componentProperties: {
+      dramas: dramas,
+    },
+  })
+})
